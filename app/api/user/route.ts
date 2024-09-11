e7a5d521449fb2c1cfd69f2331a7b6046817e69f
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2024-09-11 09:36:33 -0400

    Update route.ts

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,9 +1,10 @@
 
 import { PrismaClient } from "@prisma/client";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 const prisma = new PrismaClient();
 
-export async function POST(req: NextRequest){
+export async function POST(req: NextRequest, res: NextResponse){
+    try {
     const body = await req.json();
     prisma.user.create({
         data:{
@@ -12,6 +13,47 @@ export async function POST(req: NextRequest){
         }
     })
     return Response.json({
-        message: "User created successfully!!"
-    })
+        message: "User created successfully!!",        
+    })}catch (e){
+        NextResponse.json({
+            message: "error while creating the user"
+        })
+        console.log(e)
+    }
 }
+/*1. Get the headers
+  2. Verify the headers
+  3. Get the jwt token
+  4. Verify the token 
+  5. If the token and secret is correct than 
+  6. Pass the token to the user and let them access the information from the database.
+*/
+export default async function GET(req: NextRequest, res: NextResponse){
+    try {
+        // const headers = HEADERS || " ";
+        /* 
+            if(header){
+            NextResponse.json({
+            message: "Passed header is correct"
+            }, {status: 200})
+            }
+        */
+        const body = await req.body;
+        prisma.user.findMany({
+            where:{
+                // username: body.username,
+                // passwors: body.passwors
+            }
+        })  
+
+        NextResponse.json({
+            message: "User got the information"
+        })
+    }catch(error){
+        NextResponse.json({
+            error: "The error is :- " + error 
+        }, {status: 411})
+    }
+    
+}
+
