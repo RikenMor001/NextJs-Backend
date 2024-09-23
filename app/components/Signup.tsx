eb14f0543e83e1d5d87ca7a23493ba24ed4f0c64
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2024-09-23 09:15:12 -0400

    Update Signup.tsx

--- app/components/Signup.tsx
+++ app/components/Signup.tsx
@@ -1,3 +1,4 @@
+
 // button uses axios.post because it will post the username and the password or any other type of schema that is been introduced by the database
 // provider. Followin the " Prsima.Schema "
 "use client"
@@ -8,6 +9,25 @@ export function Signup(){
     const [password, setPassword] = useState();
 
     return <div>
-        New interface coming soon 
+        <div className="grid grid-cols-2">
+            <div>
+                
+            </div>
+            <div className="bg-slate-200 flex justify-center flex-col h-screen">
+                <div className="flex justify-center">
+                    <div className="max-w-lg">
+                        <div className="text-3xl font-bold">
+                            "The customer support i received was exceptional. The support team went above and beyond to address my concerns." 
+                        </div>
+                        <div className="mt-2 max-w-md text-lg font-semibold text-left text-slate-800">
+                            CEO | Acme corp.
+                        </div>
+                        <div className=" max-w-md text-md font-semibold text-left text-slate-500">
+                            -Riken Patel
+                        </div>
+                    </div>
+                </div>
+            </div>            
+        </div>
     </div>
 }
