861dcbd4215015441a3e815c2173cf7dd40330da
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2024-10-24 00:29:26 -0400

    Add files via upload

---
+++ prisma/migrations/20241024042729_updated_schema/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
